259807f5c3ca868d60b6f095f6e22c82625d6610
C C <cc@Cs-MacBook-Pro.local>
2026-04-26 10:12:09 +0200

    Refactor yml jobs

--- scripts/create_fsh.py
+++ scripts/create_fsh.py
@@ -75,7 +75,7 @@ def delete_directory(directory):
         print(f"Error deleting directory: {e}")
 
 def main():
-
+ 
     tactics_data = load_data("tactics_data.json")
 
     fsh_content_tacitcs = create_tactic_code_system(tactics_data)
