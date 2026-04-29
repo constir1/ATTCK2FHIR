6a91a99d5e9de405a42a3016f655a8db2371b68a
C C <cc@Cs-MacBook-Pro.local>
2026-04-29 21:52:16 +0200

    Fix csd-0 constraint

--- scripts/create_fsh.py
+++ scripts/create_fsh.py
@@ -28,8 +28,8 @@ def load_data(file_name):
 
 def create_tactic_code_system(tactics_data):
     lines = []
-    lines.append('CodeSystem: MITRE-ATTCK-Tactics')
-    lines.append('Id: MITREATT&CKTactics')
+    lines.append('CodeSystem: MITRE_ATTCK_Tactics')
+    lines.append('Id: MITREATTCKTactics')
     lines.append('Title: "CS MITRE ATT&CK Tactics"')
     lines.append('Description: "This Code System contains all MITRE ATT&CK Enterprise Tactics"')
     lines.append('')
@@ -47,8 +47,8 @@ def create_tactic_code_system(tactics_data):
 
 def create_techniques_code_system(techniques_data):
     lines = []
-    lines.append('CodeSystem: MITRE-ATTCK-Techniques')
-    lines.append('Id: MITREATT&CKTechniques')
+    lines.append('CodeSystem: MITRE_ATTCK_Techniques')
+    lines.append('Id: MITREATT_CKTechniques')
     lines.append('Title: "CS MITRE ATT&CK Techniques and Subtechniques"')
     lines.append('Description: "This Code System contains all MITRE ATT&CK Enterprise Techniques and Subtechniques with their tactic and parent-technique relationships expressed as properties."')
     lines.append('')
